8a84931566da53f7f0ca30a429d99261a026fe31
minon250 <minon250@gmail.com>
2021-12-23 11:10:17 +1100

    made init public

--- Sources/SCSportbookRacing/SCSportbookRacing.swift
+++ Sources/SCSportbookRacing/SCSportbookRacing.swift
@@ -1,7 +1,12 @@
 import SwiftUI
 
 public struct SportsbookLobbyScreen: View {
+    
+    public init(){}
    public var body: some View {
         Text("Sport book lobby for Racing")
     }
 }
+
+
+
