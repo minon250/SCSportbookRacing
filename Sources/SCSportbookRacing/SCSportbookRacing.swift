e5d5d3d47ce3e71c0166851db242bb02870b75c5
minon250 <minon250@gmail.com>
2021-12-23 11:32:44 +1100

    awda

--- Sources/SCSportbookRacing/SCSportbookRacing.swift
+++ Sources/SCSportbookRacing/SCSportbookRacing.swift
@@ -4,9 +4,22 @@ public struct SportsbookLobbyScreen: View {
     
     public init(){}
    public var body: some View {
-        Text("Sport book lobby for Racing")
+       VStack(alignment:.center) {
+           Text("Welcome to Sport Champs - Racing")
+               .font(.title)
+               .multilineTextAlignment(.center)
+           
+           Spacer()
+           Text("My implementation sits outside the current app")
+               .multilineTextAlignment(.center)
+       }
     }
 }
 
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SportsbookLobbyScreen()
+    }
+}
 
 
